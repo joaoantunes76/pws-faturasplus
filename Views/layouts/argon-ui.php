fe4701b499bea838d353081c4a1cdfc7dea503af
joaoantunes76 <2190732@my.ipleiria.pt>
2022-05-27 13:45:11 +0100

    color fix in template

--- Views/layouts/argon-ui.php
+++ Views/layouts/argon-ui.php
@@ -265,7 +265,7 @@ $controller = $route[2];
 <!-- Github buttons -->
 <script async defer src="https://buttons.github.io/buttons.js"></script>
 <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-<script src="<?= Url::getBaseUrl() ?>/public/js/argon-dashboard.min.js?v=2.0.2"></script>
+<script src="<?= Url::getBaseUrl() ?>/public/js/argon-dashboard.js"></script>
 </body>
 
 </html>
